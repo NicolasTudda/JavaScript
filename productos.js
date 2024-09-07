5aaea365eeae35ec0f687278e099a2b77f1ab65a
nicolas tudda <tototuddautlook.com>
2024-09-06 23:23:17 -0300

    ecommerce

--- productos.js
+++ productos.js
@@ -1,4 +1,7 @@
 const shopContent = document.getElementById("shopContent")
+const verCarrito = document.getElementById("verCarrito");
+const modalContainer = document.getElementById("modal-container")
+
 const productos = [
 {   id: 1,
     nombre: "torta a",
@@ -39,5 +42,55 @@ productos.forEach((product)=>{
     comprar.className = "comprar";
 
     content.append(comprar)
+
+    comprar.addEventListener("click", () =>{
+        carrito.push({
+            id : product.id,
+            img: product.img,
+            nombre: product.nombre,
+            precio: product.precio,
+        });
+    });
+});
+
+verCarrito.addEventListener("click", () => {
+    modalContainer.innerHTML = "";
+    modalContainer.style.display = "flex";
+    const modalHeader = document.createElement("div");
+    modalHeader.className = "modal-header";
+    modalHeader.innerHTML = `
+    <h1 class="modal-header-title">Carrito.</h1>
+    `;
+    modalContainer.append(modalHeader);
+
+    const modalbutton = document.createElement("h1");
+    modalbutton.innerText = "x";
+    modalbutton.className = "modal-header-button";
+
+    modalbutton.addEventListener("click", () => {
+        modalContainer.style.display = "none";
+    });
+
+    modalHeader.append(modalbutton);
+
+    carrito.forEach((product) => {
+    
+        let carritoContent = document.createElement("div")
+        carritoContent.className = "modal-content"
+        carritoContent.innerHTML = `
+        <img src="${product.img}">
+        <h3>${product.nombre}</h3>
+        <p>${product.precio} $</p>
+    `;
+
+    modalContainer.append(carritoContent)
+    }) 
+    
+    const total = carrito.reduce((acc,el) => acc + el.precio, 0);
+
+    const totalBuying = document.createElement("div")
+    totalBuying.className = "total-content"
+    totalBuying.innerHTML = `total a pagar: ${total} $`;
+    modalContainer.append(totalBuying);
 });
 
